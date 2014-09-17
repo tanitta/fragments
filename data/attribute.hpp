00c4d5fb1bebf646fe2e760835c11236a7ef28fa
tanitta <ttata.trit@gmail.com>
2014-09-18 00:14:06 +0900

    Add field math_

--- data/attribute.hpp
+++ data/attribute.hpp
@@ -1,10 +1,13 @@
 #pragma once
-
+#include <boost/numeric/ublas/matrix.hpp>
 namespace fragments {
 	namespace data {
 		class Attribute {
 			public:
-				Attribute(){};
+				const float mass_;
+				const boost::numeric::ublas::matrix<float> matrix_;
+				Attribute():mass_(25),
+				matrix_(3,3){};
 				virtual ~Attribute(){};
 		};
 	} // namespace data
