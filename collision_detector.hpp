32d2ddac0ebc538bb9880d22a224aaf01046db9f
tanitta <ttata.trit@gmail.com>
2014-09-19 00:33:04 +0900

    Enable to sort StaticEntities

--- collision_detector.hpp
+++ collision_detector.hpp
@@ -3,6 +3,19 @@
 #include <data/static_entity.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 namespace  {
+	class LessCenter {
+		private:
+			const int axis_;
+		public:
+			LessCenter(const int axis):axis_(axis){};
+			virtual ~LessCenter(){};
+
+			bool operator()(fragments::data::StaticEntity* a, fragments::data::StaticEntity* b) {
+				return a->GetCenter()[0] < b->GetCenter()[0];
+			};
+
+	};
+
 	class StaticNode {
 		public:
 			std::vector<StaticNode*> next_ptrs_;
@@ -21,15 +34,17 @@ namespace  {
 				};
 			};
 
+
 			void MakeNord(std::vector<fragments::data::StaticEntity*> static_entity_ptrs){
-				// std::vector<fragments::data::StaticEntity> static_entities = static_entities_ptr;
-				std::cout<<static_entity_ptrs.size()<<std::endl;
-				// if (static_entities_ptr->size() > 1){
-				//
-				// }else{
-				//
-				// };
+				std::cout<<static_entity_ptrs[0]->GetCenter()[0]<<std::endl;
+				if (static_entity_ptrs.size() > 1){
+					LessCenter less_center(0);
+					std::sort(static_entity_ptrs.begin(), static_entity_ptrs.end(), less_center);
+				}else{
+
+				};
 			};
+
 	};
 } // namespace
 
@@ -44,9 +59,11 @@ namespace fragments {
 
 			void Setup(std::vector<fragments::data::StaticEntity>* static_entities_ptr){
 				std::vector<fragments::data::StaticEntity*> static_entity_ptrs;
+
 				for (int i = 0; i < static_entities_ptr->size(); i++) {
 					static_entity_ptrs.push_back(&static_entities_ptr->operator[](i));
 				}
+
 				static_tree_.Setup(static_entity_ptrs);
 			};
 
