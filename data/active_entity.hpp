c3f92439e851914824c792d0703054235c9cd4c6
tanitta <ttata.trit@gmail.com>
2014-09-20 15:34:05 +0900

    Remove ActiveEntity::static_entity_ptrs

--- data/active_entity.hpp
+++ data/active_entity.hpp
@@ -11,19 +11,11 @@ namespace fragments {
 				fragments::data::Attribute attribute_;
 				fragments::data::State state_;
 
-				vector<fragments::data::StaticEntity*> static_entity_ptrs_;
-
-				ActiveEntity(fragments::data::Shape shape):
+				ActiveEntity(fragments::data::Shape shape = fragments::data::Shape::SQUARE):
 					shape_(shape),
 					state_(),
-					attribute_(),
-					static_entity_ptrs_(0){};
+					attribute_(){};
 				virtual ~ActiveEntity(){};
-
-				void PushStaticEntity(fragments::data::StaticEntity* ptr){
-					static_entity_ptrs_.push_back(ptr);
-				};
-
 		};
 	} // namespace data
 } // namespace fragments
