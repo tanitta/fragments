e071313b8339f980aac4b98e93bd526451b55bc1
tanitta <ttata.trit@gmail.com>
2016-03-22 21:09:23 +0900

    Add constraintsolver.d

---
+++ source/fragments/constraintsolver.d
@@ -0,0 +1,18 @@
+module fragments.constraintsolver;
+
+import fragments.constraintpair;
+
+/++
++/
+class ConstraintSolver(NumericType){
+	alias N = NumericType;
+	public{
+		void solve(ref ConstraintPair!N[] constraintPairs){
+			
+		}
+	}//public
+
+	private{
+	}//private
+}//class ConstraintSolver
+
