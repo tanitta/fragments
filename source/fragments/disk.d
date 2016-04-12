5a8b40287a96c0bf436dd70296c3dcf5b852dd5c
tanitta <ttata.trit@gmail.com>
2016-04-12 20:34:06 +0900

    Add disk.

---
+++ source/fragments/disk.d
@@ -0,0 +1,22 @@
+module fragments.disk;
+
+import fragments.material;
+/++
++/
+class Disk(NumericType) : DynamicEntity!(NumericType){
+	alias N = NumericType;
+	alias V3 = ar.Vector!(N, 3);
+	alias Q = ar.Quaternion!(N);
+	mixin DynamicEntityProperties!(N);
+	
+	public{
+		this(in Material!N m, in N radius = N(1.0)){
+			_material = m;
+			
+		}
+	}//public
+
+	private{
+		Material!N _material;
+	}//private
+}//class Disk
